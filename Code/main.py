7905e3234eaf22b7981f956f30949716b5559d08
Pārsla Bogdanova <parslabog@gmail.com>
2025-11-30 19:25:43 +0200

    Nomainot windows izmēru no 1288->720, vienāds ar augstuma izmēru. dt ir delta laiks, starp diviem "frame" spēlē, neatkarībā no FPS spēlēs.
    clock.tick(60)/1000. clock.tick(60) mēģinot ierobežot FPS līdz 60, atgriežot ms(milisekundes). Dalot ar 1000 iegūst sekundēs = 0.016s(apmēram 6- FPS)

--- Code/main.py
+++ Code/main.py
@@ -1,24 +1,45 @@
 import pygame
-from OOP.Circle import Circle
+from OOP.Game import Game
 
 pygame.init()
-WINDOW_WIDTH, WINDOW_HEIGHT = 1288, 720
+WINDOW_WIDTH, WINDOW_HEIGHT = 720, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Pastāvīgais darbs")
+
+clock = pygame.time.Clock()
+game = Game()
 running = True
-circle = Circle()
 
 while running:
+    dt = clock.tick(60)/1000
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
-            if circle.is_clicked(mouse_pos):
-                print("Hit")
+            if not game.game_over:
+                mouse_pos = pygame.mouse.get_pos()
+                if game.circle.is_clicked(mouse_pos):
+                    print("Hit")
+                    game.circle.hit_count += 1
+                    game.circle.level_up()
+
+    if not game.game_over:
+        game.circle.update(dt)
+        game.check_miss()
 
     display_surface.fill("white")
-    circle.draw(display_surface)
+
+    if not game.game_over:
+        game.circle.draw(display_surface)
+    else:
+        font = pygame.font.SysFont(None, 60)
+        text = font.render("Game over", True, (133, 197, 232))
+        text_rect = text.get_rect()
+        text_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+        display_surface.blit(text, text_rect)
+
     pygame.display.update()
 
 pygame.quit()
