5b89b872aa84153823528b49b98d39d407c518ad
Pārsla Bogdanova <parslabog@gmail.com>
2025-11-30 19:18:54 +0200

    Pievienota jauna fukcija "level_up" un "update", tiek pievienoti jauni atribūti: start_radius, shrink_speed, hit_count, hint_levels_after_clicking. Mērķis bija izveidots apli/apļus, kad lietotājs ilgā laikā nenoklīšķina uz apli, aplis samazinās. Jo vairāk ka >= 5 tiek noklišķināti, ātrums palileinās ik pa 5. Rezultātā, arvien ātrāk aplis samazinās.

--- Code/OOP/Circle.py
+++ Code/OOP/Circle.py
@@ -4,28 +4,43 @@ import math
 
 
 class Circle():
-    def __init__(self, radius=20, color=(0, 0, 0), window_width=1288, window_height=720):
+    def __init__(self, radius=100, color=(0, 0, 0), window_width=720, window_height=720):
         self.radius = radius
+        self.start_radius = radius
         self.color = color
         self.x = 0
         self.y = 0
         self.window_width = window_width
         self.window_height = window_height
+
+        self.shrink_speed = 10
+
+        self.hit_count = 0
+        self.hit_levels_after_clicking = 5
+
         self.clicked = False
         self.spawn()
 
+    def update(self, dt):
+        self.radius -= self.shrink_speed * dt
+        if self.radius < 0:
+            print("miss")
+            self.radius = 0
+
     def spawn(self):
+        self.radius = self.start_radius
         self.x = random.randint(self.radius, self.window_width - self.radius)
         self.y = random.randint(self.radius, self.window_height - self.radius)
 
     def draw(self, surface):
         pygame.draw.circle(surface, self.color,
-                           (self.x, self.y), self.radius)
+                           (self.x, self.y), int(self.radius), width=3)
 
     def is_clicked(self, mouse_pos):
         dx = mouse_pos[0] - self.x
         dy = mouse_pos[1] - self.y
         distance = math.sqrt(dx**2 + dy**2)
+
         if distance <= self.radius:
             self.clicked = True
             self.spawn()
@@ -33,3 +48,8 @@ class Circle():
         else:
             self.clicked = False
             return False
+
+    def level_up(self):
+        if self.hit_count % self.hit_levels_after_clicking == 0:
+            self.shrink_speed += 5
+            print(f"Speed: {self.shrink_speed}")
