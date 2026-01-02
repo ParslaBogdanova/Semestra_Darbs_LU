62d169f7cca8408f3cbc226086eb30462039167a
Pārsla Bogdanova <parslabog@gmail.com>
2026-01-02 02:34:56 +0200

    removed the scatter diagram, used for experiment, tried to push into hist diagram, didnt work

--- Code/OOP/Analysis.py
+++ Code/OOP/Analysis.py
@@ -22,7 +22,7 @@ class Analysis():
         mu = np.mean(data)
         sigma = np.std(data)
         x = np.linspace(min(data), max(data), 100)
-        y = norm.pdf(x, mu, sigma)
+        y = norm.pdf(x, mu, sigma) * len(data) * (max(data)-min(data))/25
         return x, y
 
     def show_results(self):
@@ -33,28 +33,21 @@ class Analysis():
 
             plt.figure(figsize=(12, 8))
 
-            plt.subplot(2, 2, 1)
+            plt.subplot(1, 2, 1)
             plt.title("Attālums no apļa centra")
             plt.xlabel("Attālums(px)")
-            plt.ylabel("???")
-            plt.hist(self.distances, density=True, bins=25, alpha=0.6,
+            plt.ylabel("Skaits")
+            plt.hist(self.distances, density=False, bins=25, alpha=0.6,
                      edgecolor="black", color="#c0a0db")
             plt.plot(x_d, y_d, 'r', linewidth=2)
 
-            plt.subplot(2, 2, 2)
+            plt.subplot(1, 2, 2)
             plt.title("Reakcijas laiks")
             plt.xlabel("Reakcijas laiks(ms)")
-            plt.ylabel("???")
-            plt.hist(self.times, bins=25, density=True,
+            plt.ylabel("Skaits")
+            plt.hist(self.times, bins=25, density=False,
                      alpha=0.6, edgecolor="black", color="#a0d1db")
             plt.plot(x_t, y_t, 'r', linewidth=2)
-
-            plt.subplot(2, 1, 2)
-            plt.scatter(self.times, self.distances, c="#a1f29b",
-                        alpha=0.6, edgecolor="black")
-            plt.title("Reakcijas laiks pret attālumu no centra")
-            plt.xlabel("Reakcijas laiks(ms)")
-            plt.ylabel("Attālums(ms)")
 
             plt.tight_layout()
             pdf.savefig()
